bf9cf0cee7cc18f9863ab75f7eb6607c01f6ce8e
wharrington12 <wharrin3@jhu.edu>
2018-09-28 17:05:33 -0400

    Changes to less, function to resize query window, function to detect if user is using iphone and redistribute buttons accordingly.

--- includes/form-dr14.php
+++ includes/form-dr14.php
@@ -15,13 +15,9 @@ $result .= <<<EOT
 <h2><a role="button" data-toggle="collapse" href="#sqls-form" aria-expanded="true" aria-controls="sqls-form">SQL Search</a></h2>
 <div class="form sqls-form collapse show">
 <form id="sqls-form">
-<table width=100%>
-<tr>
-<td><textarea id="sqls-query" name="cmd" class="sqls-query" rows=10 cols=60 disabled>SELECT TOP 10 p.objid,p.ra,p.dec,p.u,p.g,p.r,p.i,p.z,p.run, p.rerun, p.camcol, p.field, s.specobjid, s.class, s.z as redshift,s.plate, s.mjd, s.fiberid FROM PhotoObj AS p JOIN SpecObj AS s ON s.bestobjid = p.objid WHERE p.u BETWEEN 0 AND 19.6 AND g BETWEEN 0 AND 20</textarea></td>
-<td><button id="sqls-edit" name="sqls-edit" class ="sqls-edit btn btn-primary" data-unlock="yes">Edit</button></td>
-</tr>
-</table>
-<table width=100%>
+<button id="sqls-edit" name="sqls-edit" class ="sqls-edit btn btn-primary" data-unlock="yes">Edit</button>
+<textarea id="sqls-query" name="cmd" class="sqls-query" data-colnum=60 rows=10 cols=60 disabled>SELECT TOP 10 p.objid,p.ra,p.dec,p.u,p.g,p.r,p.i,p.z,p.run, p.rerun, p.camcol, p.field, s.specobjid, s.class, s.z as redshift,s.plate, s.mjd, s.fiberid FROM PhotoObj AS p JOIN SpecObj AS s ON s.bestobjid = p.objid WHERE p.u BETWEEN 0 AND 19.6 AND g BETWEEN 0 AND 20</textarea>
+<table>
 <tr>	
 <td><button id="sqls-submit" name="sqls-submit" class="sqls-submit btn btn-primary">Submit</button></td>
 <td><button id="sqls-syntax" name="sqls-syntax" data-sqls-submitto="http://skyserver.sdss.org/dr14/en/tools/search/x_results.aspx?searchtool=SQL&TaskName=Skyserver.Search.SQL&ReturnHtml=true&format=html&syntax=Syntax&cmd=" class="sqls-syntax btn btn-secondary">Check Syntax</button></td>
